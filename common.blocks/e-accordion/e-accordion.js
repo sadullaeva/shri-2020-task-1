f280f5944dbb263fab7336442a9c82428f58e98a
Marina Sadullaeva <marina.sadullaeva@7bits.it>
2020-01-03 23:40:07 +0600

    Added e-accordion block

---
+++ common.blocks/e-accordion/e-accordion.js
@@ -0,0 +1,16 @@
+function toggleAccordion(evt) {
+  const short = evt.target.closest('.e-accordion__short');
+  if (!short) return;
+
+  const accordion = short.closest('.e-accordion');
+  const more = accordion && accordion.querySelector('.e-accordion__more');
+  if (!more) return;
+
+  if (more.style.display === '' || more.style.display === 'none') {
+    more.style.display = 'unset';
+  } else {
+    more.style.display = 'none';
+  }
+}
+
+document.body.addEventListener('click', toggleAccordion);
