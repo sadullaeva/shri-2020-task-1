007af62c583d87d9082f46e8b5b7d68ed5eec546
Marina Sadullaeva <marina.sadullaeva@7bits.it>
2020-01-04 23:59:51 +0600

    Added onoffswitch

---
+++ content.blocks/onoffswitch/onoffswitch.js
@@ -0,0 +1,19 @@
+function toggleTheme(evt) {
+  const onoffswitch = evt.target.closest('.onoffswitch');
+  if (!onoffswitch) return;
+
+  onoffswitch.classList.toggle('onoffswitch_checked');
+
+  const defaultClassName = 'theme_color_project-default';
+  const inverseClassName = 'theme_color_project-inverse';
+  const themedElements = document.querySelectorAll(`.${defaultClassName}, .${inverseClassName}`);
+
+  if (themedElements) {
+    for (let i = 0; i < themedElements.length; i++) {
+      themedElements[i].classList.toggle(defaultClassName);
+      themedElements[i].classList.toggle(inverseClassName);
+    }
+  }
+}
+
+document.body.addEventListener('click', toggleTheme);
